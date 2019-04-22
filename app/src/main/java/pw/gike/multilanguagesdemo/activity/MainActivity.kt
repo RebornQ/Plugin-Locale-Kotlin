faf48cbe2dc1a676afc73608e0518c6e25539099
Reborn <ren_xiaoyao@outlook.com>
2019-04-22 15:46:13 +0800

    1.Add show app name from PackageManager(not strings). 2.Add test string from application and activity.

--- app/src/main/java/pw/gike/multilanguagesdemo/activity/MainActivity.kt
+++ app/src/main/java/pw/gike/multilanguagesdemo/activity/MainActivity.kt
@@ -1,18 +1,27 @@
 package pw.gike.multilanguagesdemo.activity
 
 import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import pw.gike.multilanguagesdemo.App
 import pw.gike.multilanguagesdemo.R
 import pw.gike.multilanguagesdemo.utils.LocaleManageUtil
 
 class MainActivity : BaseActivity() {
 
     private lateinit var btStartSettingActivity: Button
+    private lateinit var btTestActivityString: Button
+    private lateinit var btTestApplicationString: Button
 
+    private lateinit var tvAppName: TextView
     private lateinit var tvSystemLanguage: TextView
     private lateinit var tvUserSelectLanguage: TextView
+    private lateinit var tvApplicationString: TextView
+    private lateinit var tvActivityString: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +32,31 @@ class MainActivity : BaseActivity() {
 
     private fun initView() {
         btStartSettingActivity = findViewById(R.id.bt_settings)
+        btTestApplicationString = findViewById(R.id.bt_test_application_string)
+        btTestActivityString = findViewById(R.id.bt_test_activity_string)
 
+        tvAppName = findViewById(R.id.tv_app_name)
         tvSystemLanguage = findViewById(R.id.tv_system_language)
         tvUserSelectLanguage = findViewById(R.id.tv_select_language)
+        tvApplicationString = findViewById(R.id.tv_application_string)
+        tvActivityString = findViewById(R.id.tv_activity_string)
 
         btStartSettingActivity.setOnClickListener {
             val intent = Intent(this, SettingActivity::class.java)
             startActivity(intent)
         }
+        btTestApplicationString.setOnClickListener {
+            tvApplicationString.text = App.instance.getString(R.string.application_string, App.instance.getString(R.string.test_success))
+            Toast.makeText(App.instance, App.instance.getString(R.string.test_success), Toast.LENGTH_SHORT).show()
+        }
+        btTestActivityString.setOnClickListener {
+            tvActivityString.text = getString(R.string.activity_string, getString(R.string.test_success))
+            Toast.makeText(this, getString(R.string.test_success), Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun setValue() {
+        tvAppName.text = App.instance.getString(R.string.app_name_label, getApplicationName())
         val string = getString(
             R.string.system_language,
             LocaleManageUtil.getSystemLocale(this).displayLanguage
@@ -43,5 +66,19 @@ class MainActivity : BaseActivity() {
             R.string.user_select_language,
             LocaleManageUtil.getSelectLanguage(this)
         )
+        tvApplicationString.text = App.instance.getString(R.string.application_string, App.instance.getString(R.string.test_success))
+        tvActivityString.text = getString(R.string.activity_string, getString(R.string.test_success))
+    }
+
+    private fun getApplicationName() : String {
+        var packageManager : PackageManager? = null
+        var applicationInfo : ApplicationInfo? = null
+        try {
+            packageManager = applicationContext.packageManager
+            applicationInfo = packageManager.getApplicationInfo(packageName, 0)
+        } catch (e : PackageManager.NameNotFoundException) {
+        }
+        val applicationName = packageManager!!.getApplicationLabel(applicationInfo)
+        return applicationName.toString()
     }
 }
