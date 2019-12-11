f71a6aa113e245cd17903e7154e37574ceeeae05
Reborn <ren_xiaoyao@outlook.com>
2019-12-12 02:59:17 +0800

    add LocalePlugin#terminate(...) function. When app is terminating, to unregister some object we register (Not tested yet)

--- plugin-locale/src/main/java/com/mallotec/reb/localeplugin/LocalePlugin.kt
+++ plugin-locale/src/main/java/com/mallotec/reb/localeplugin/LocalePlugin.kt
@@ -11,21 +11,48 @@ import com.mallotec.reb.localeplugin.utils.LocaleHelper
  */
 object LocalePlugin {
 
-    private fun setLifecycleCallbacks(application: Application) {
-        application.registerActivityLifecycleCallbacks(LocaleActivityLifecycleCallbacks())
-        application.registerComponentCallbacks(LocaleComponentLifecycleCallbacks(application))
+    private var localeActivityLifecycleCallbacks: LocaleActivityLifecycleCallbacks? = null
+    private var localeComponentLifecycleCallbacks: LocaleComponentLifecycleCallbacks?  = null
+
+    // 注册 LifecycleCallbacks
+    private fun registerLifecycleCallbacks(application: Application) {
+        localeActivityLifecycleCallbacks = LocaleActivityLifecycleCallbacks()
+        localeComponentLifecycleCallbacks = LocaleComponentLifecycleCallbacks(application)
+        application.registerActivityLifecycleCallbacks(localeActivityLifecycleCallbacks)
+        application.registerComponentCallbacks(localeComponentLifecycleCallbacks)
+    }
+
+    // 注销 LifecycleCallbacks
+    private fun unregisterLifecycleCallbacks(application: Application) {
+        localeActivityLifecycleCallbacks?.let {
+            application.unregisterActivityLifecycleCallbacks(it)
+            localeActivityLifecycleCallbacks = null
+        }
+        localeComponentLifecycleCallbacks?.let {
+            application.unregisterComponentCallbacks(it)
+            localeComponentLifecycleCallbacks = null
+        }
     }
 
     fun init(application: Application, updateInterfaceWay: Int = LocaleConstant.RECREATE_CURRENT_ACTIVITY): LocalePlugin {
         return this.apply {
+
+            // 初始化所有用到的工具类
             ActivityHelper.init(updateInterfaceWay)
             LocaleDefaultSPHelper.init(application)
             LocaleHelper.init(application)
 
-            setLifecycleCallbacks(application)
+            registerLifecycleCallbacks(application)
+
+            // 缓存系统语言
             LocaleHelper.getInstance().cacheSystemLocale()
             // 设置语言
             LocaleHelper.getInstance().updateApplicationContext(application)
         }
+    }
+
+    // App 结束时调用
+    fun terminate(application: Application) {
+        unregisterLifecycleCallbacks(application)
     }
 }
