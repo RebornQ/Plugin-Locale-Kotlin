c03d6f1f82550bf467423476b246242eb98c8a32
Reborn <ren_xiaoyao@outlook.com>
2019-12-12 18:06:45 +0800

    add BroadcastReceiver null judgement when add it

--- plugin-locale/src/main/java/com/mallotec/reb/localeplugin/utils/BroadcastReceiverManager.kt
+++ plugin-locale/src/main/java/com/mallotec/reb/localeplugin/utils/BroadcastReceiverManager.kt
@@ -3,25 +3,32 @@ package com.mallotec.reb.localeplugin.utils
 import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.IntentFilter
+import android.util.Log
 
 /**
  * Created by reborn on 2019-12-12.
  */
 object BroadcastReceiverManager {
 
+    private const val TAG = "ReceiverManager"
+
     var receiverMap : HashMap<Activity, BroadcastReceiver> = HashMap()
         private set
 
     fun registerBroadcastReceiver(activity: Activity, receiver: BroadcastReceiver, intentFilter: IntentFilter) {
-        activity.registerReceiver(receiver, intentFilter)
-        receiverMap[activity] = receiver
+        if (receiverMap[activity] == null) {
+            activity.registerReceiver(receiver, intentFilter)
+            receiverMap[activity] = receiver
+        } else {
+            Log.e(TAG, activity.localClassName + "#BroadcastReceiver is already registered")
+        }
     }
 
     fun unregisterBroadcastReceiver(activity: Activity) {
         receiverMap[activity]?.let {
             activity.unregisterReceiver(it)
             receiverMap.remove(activity)
-        }
+        } ?: Log.e(TAG, activity.localClassName + "#BroadcastReceiver is already unregistered")
     }
 
     private fun unregisterAllBroadcastReceiver() {
