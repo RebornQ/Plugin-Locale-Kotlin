b3d3218c52015142453e9f7d49fd6d4b9b1cf789
Reborn <ren_xiaoyao@outlook.com>
2019-12-12 19:29:18 +0800

    Update ComponentCallbacks to ComponentCallbacks2

--- plugin-locale/src/main/java/com/mallotec/reb/localeplugin/lifecycle/LocaleComponentLifecycleCallbacks.kt
+++ plugin-locale/src/main/java/com/mallotec/reb/localeplugin/lifecycle/LocaleComponentLifecycleCallbacks.kt
@@ -1,6 +1,6 @@
 package com.mallotec.reb.localeplugin.lifecycle
 
-import android.content.ComponentCallbacks
+import android.content.ComponentCallbacks2
 import android.content.Context
 import android.content.res.Configuration
 import com.mallotec.reb.localeplugin.utils.LocaleHelper
@@ -8,9 +8,11 @@ import com.mallotec.reb.localeplugin.utils.LocaleHelper
 /**
  * Created by reborn on 2019-12-11.
  */
-class LocaleComponentLifecycleCallbacks(private val context: Context)  : ComponentCallbacks {
+class LocaleComponentLifecycleCallbacks(private val context: Context)  : ComponentCallbacks2 {
 
     override fun onLowMemory() { }
+
+    override fun onTrimMemory(level: Int) { }
 
     override fun onConfigurationChanged(newConfig: Configuration) {
         LocaleHelper.getInstance().onConfigurationChanged(context)
